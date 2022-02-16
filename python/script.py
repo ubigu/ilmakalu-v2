edd771d227b1f114aa5ef82a706fad9e83a7d065
Ville Koivisto <ville.koivisto@ubigu.fi>
2022-02-16 10:29:37 +0200

    Refined dictionary and changes to insert into

--- python/script.py
+++ python/script.py
@@ -2,9 +2,32 @@ import psycopg2
 import json
 import requests
 
+'''
+Käyttövoimakoodit:
+- Bensiini = 01
+- Diesel = 02
+- Polttoöljy = 03
+- Sähkö = 04
+- Vety = 05
+- Kaasu = 06
+- Nestekaasu (LPG) = 11
+- Maakaasu (CNG) = 13
+- Bensiini/Puu = 33
+- Bensiini + moottoripetroli = 34
+- Etanoli = 37
+- Bensiini/CNG = 38
+- Bensiini/Sähkö (ladattava hybridi) = 39
+- Bensiini/Etanoli = 40
+- Bensiini/Metanoli = 41
+- Bensiini/LPG = 42
+- Diesel/CNG = 43
+- Diesel/Sähkö (ladattava hybridi) = 44
+- Muu = Y
+- Yhteensä = YH
+'''
+
 # Ask municipality code from user
 municipality_code = "KU" + input("Give municipality code in 3-number format (add leading zeros): ")
-#municipality_code = "KU091"
 
 # Access and read JSON-query file produced in https://trafi2.stat.fi/PXWeb/pxweb/fi/TraFi/ from the same folder
 with open("query.json", "r", encoding="utf-8") as file:
@@ -24,22 +47,38 @@ session = requests.Session()
 response = session.post(url, json=query)
 response_json = json.loads(response.content.decode('utf-8-sig'))
 
-# Create a dictionary for key value pairs
-energy_modes = {}
+# Create a initial dictionary for key value pairs
+energy_modes_init = {}
 
 # loop through the response and save to dictionary
 for i in response_json['data']:
     key = i["key"][3]
     value = i["values"][0]
     if value == "-":
-        energy_modes[key] = 0
+        energy_modes_init[key] = 0
     else:
-        energy_modes[key] = value
+        energy_modes_init[key] = value
     
-# Check dictionary contents
-print(energy_modes)
+# Check initial dictionary contents
+#print(energy_modes_init)
+
+# Create a refined dictionary with proper key names, energy mode combinations and proportional shares
+energy_modes_ref = {}
+energy_modes_ref["kvoima_bensiini"] = int(energy_modes_init["01"]) / int(energy_modes_init["YH"])
+energy_modes_ref["kvoima_diesel"] = int(energy_modes_init["02"]) / int(energy_modes_init["YH"])
+energy_modes_ref["kvoima_etanoli"] = int(energy_modes_init["37"]) / int(energy_modes_init["YH"])
+energy_modes_ref["kvoima_kaasu"] = (int(energy_modes_init["06"]) + int(energy_modes_init["11"]) + int(energy_modes_init["13"])) / int(energy_modes_init["YH"])
+energy_modes_ref["kvoima_phev_b"] = int(energy_modes_init["39"]) / int(energy_modes_init["YH"])
+energy_modes_ref["kvoima_phev_d"] = int(energy_modes_init["44"]) / int(energy_modes_init["YH"])
+energy_modes_ref["kvoima_ev"] = int(energy_modes_init["04"]) / int(energy_modes_init["YH"])
+energy_modes_ref["kvoima_vety"] = int(energy_modes_init["05"]) / int(energy_modes_init["YH"])
+energy_modes_ref["kvoima_muut"] = (int(energy_modes_init["33"]) + int(energy_modes_init["34"]) + int(energy_modes_init["38"]) + int(energy_modes_init["40"]) + int(energy_modes_init["41"]) + int(energy_modes_init["42"]) + int(energy_modes_init["43"]) + int(energy_modes_init["Y"])) / int(energy_modes_init["YH"])
+
+# Check refined dictionary contents
+print(energy_modes_ref)
 
 # Add dictionary contents to database
+
 # Connect to database
 conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=Teemo90 port=5432")
 cursor = conn.cursor()
@@ -49,16 +88,28 @@ cursor.execute("DROP TABLE IF EXISTS energy_modes")
 create_table ='''CREATE TABLE energy_modes(
    id SERIAL PRIMARY KEY,
    municipality TEXT,
-   energy_mode TEXT,
-   amount int
+   kvoima_bensiini DECIMAL(10,3),
+   kvoima_diesel DECIMAL(10,3),
+   kvoima_etanoli DECIMAL(10,3),
+   kvoima_kaasu DECIMAL(10,3),
+   kvoima_phev_b DECIMAL(10,3),
+   kvoima_phev_d DECIMAL(10,3),
+   kvoima_ev DECIMAL(10,3),
+   kvoima_vety DECIMAL(10,3),
+   kvoima_muut DECIMAL(10,3)
 )'''
 cursor.execute(create_table)
 
-# Add data to the table
-for key, value in energy_modes.items():
-    cursor.execute("""INSERT INTO energy_modes(municipality, energy_mode, amount) 
-                  VALUES (%s, %s, %s);""", (municipality_code, key, value))
+# Insert values from python dictionary to the table
+insert_into = ("""INSERT INTO energy_modes(municipality, kvoima_bensiini, kvoima_diesel, kvoima_etanoli, kvoima_kaasu, kvoima_phev_b, kvoima_phev_d, 
+              kvoima_ev, kvoima_vety, kvoima_muut) 
+              VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""")
 
+cursor.execute(insert_into, (municipality_code, energy_modes_ref["kvoima_bensiini"], energy_modes_ref["kvoima_diesel"], energy_modes_ref["kvoima_etanoli"], 
+              energy_modes_ref["kvoima_kaasu"], energy_modes_ref["kvoima_phev_b"], energy_modes_ref["kvoima_phev_d"], energy_modes_ref["kvoima_ev"],
+              energy_modes_ref["kvoima_vety"], energy_modes_ref["kvoima_muut"]))
+
+# Finalize
 conn.commit()
 cursor.close()
 conn.close()
