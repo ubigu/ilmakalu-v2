e81a02bbbd8fc84466be9fcda0dea869abc9c6fe
Ville Koivisto <ville.koivisto@ubigu.fi>
2022-02-18 15:26:46 +0200

    Initial additions for including heavy cars + area code module
    - Differentiating between passenger cars and heavy cars
    - Area code handler module
    - Separated energy mode codes to a separate markdown file

---
+++ python/energy_mode_codes_traficom.md
@@ -0,0 +1,26 @@
+# Energy modes
+
+Naming in Finnish. 
+
+| Energy mode | Code |
+| ----------- | ----------- |
+| Bensiini | 01 |
+| Diesel | 02 |
+| Polttoöljy | 03 |
+| Sähkö | 04 |
+| Vety | 05 |
+| Kaasu | 06 |
+| Nestekaasu (LPG) | 11 |
+| Maakaasu (CNG) | 13 |
+| Bensiini/Puu | 33 |
+| Bensiini + moottoripetroli | 34 |
+| Etanoli | 37 |
+| Bensiini/CNG | 38 |
+| Bensiini/Sähkö (ladattava hybridi) | 39 |
+| Bensiini/Etanoli | 40 |
+| Bensiini/Metanoli | 41 |
+| Bensiini/LPG | 42 |
+| Diesel/CNG | 43 |
+| Diesel/Sähkö (ladattava hybridi) | 44 |
+| Muu | Y |
+| Yhteensä | YH |
--- python/get_automobile_energy_mode_dist.py
+++ python/get_automobile_energy_mode_dist.py
@@ -2,105 +2,103 @@ import psycopg2
 import json
 import requests
 from config.credentials import Config
+from area_code_handler import mun_code_handler, reg_code_handler
 
 # Create a config object
 cfg = Config()
 cfg.user_credentials('database')
 
-'''
-Käyttövoimakoodit:
-- Bensiini = 01
-- Diesel = 02
-- Polttoöljy = 03
-- Sähkö = 04
-- Vety = 05
-- Kaasu = 06
-- Nestekaasu (LPG) = 11
-- Maakaasu (CNG) = 13
-- Bensiini/Puu = 33
-- Bensiini + moottoripetroli = 34
-- Etanoli = 37
-- Bensiini/CNG = 38
-- Bensiini/Sähkö (ladattava hybridi) = 39
-- Bensiini/Etanoli = 40
-- Bensiini/Metanoli = 41
-- Bensiini/LPG = 42
-- Diesel/CNG = 43
-- Diesel/Sähkö (ladattava hybridi) = 44
-- Muu = Y
-- Yhteensä = YH
-'''
-
 # Ask municipality code from user
 mun_code = int(input("Give municipality code: "))
 
-# Access and read passenger car JSON-query file produced in https://trafi2.stat.fi/PXWeb/pxweb/fi/TraFi/ from the same folder
-with open("passenger_car_query.json", "r", encoding="utf-8") as file:
-    content = file.read()
-    query = json.loads(content)
+# Ask region code from user
+reg_code = int(input("Give region code: "))
 
-# Replace municipality code with the user given to passenger car JSON query
-json_mun_code = ""
-if len(str(mun_code)) == 1:
-    json_mun_code = "KU00" + str(mun_code)
-elif len(str(mun_code)) == 2:
-    json_mun_code = "KU0" + str(mun_code)
-else:
-    json_mun_code = "KU" + str(mun_code)
+# Access and read preloaded JSON queries retrieved from https://trafi2.stat.fi/
+with open("passenger_cars_query.json", "r", encoding="utf-8") as passenger_cars, open("heavy_cars_query.json", "r", encoding="utf-8") as heavy_cars:
+    passenger_cars_content = passenger_cars.read()
+    passenger_cars_query = json.loads(passenger_cars_content)
 
-print(json_mun_code)
+    heavy_cars_content = heavy_cars.read()
+    heavy_cars_query = json.loads(heavy_cars_content)
 
-query['query'][0]['selection']['values'][0] = json_mun_code
+# Replace area codes to both queries
+passenger_cars_query['query'][0]['selection']['values'][0] = mun_code_handler(mun_code)
+heavy_cars_query['query'][0]['selection']['values'][0] = reg_code_handler(reg_code)
 
-# Rewrite the passenger car energy mode distribution JSON query file
-with open("passenger_car_query.json", 'w', encoding="utf-8") as file:
-    file.write(json.dumps(query))
-    
+# Rewrite both JSON queries <-- This needs to be replaced so we retain original JSON files
+with open("passenger_cars_query.json", 'w', encoding="utf-8") as passenger_cars_rewrite, open("heavy_cars_query.json", "w", encoding="utf-8") as heavy_cars_rewrite:
+    passenger_cars_rewrite.write(json.dumps(passenger_cars_query))
+    heavy_cars_rewrite.write(json.dumps(heavy_cars_query))
+
 # Get passenger car JSON response (POST with edited JSON query)
-url = "https://trafi2.stat.fi:443/PXWeb/api/v1/fi/TraFi/Liikennekaytossa_olevat_ajoneuvot/010_kanta_tau_101.px"
+url_passenger_cars = "https://trafi2.stat.fi:443/PXWeb/api/v1/fi/TraFi/Liikennekaytossa_olevat_ajoneuvot/010_kanta_tau_101.px"
+url_heavy_cars = "https://trafi2.stat.fi:443/PXWeb/api/v1/fi/TraFi/Liikennekaytossa_olevat_ajoneuvot/040_kanta_tau_104.px"
+
 session = requests.Session()
-response = session.post(url, json=query)
-response_json = json.loads(response.content.decode('utf-8-sig'))
-#print(response_json)
 
-# Create a initial dictionary for key value pairs for passenger cars
-energy_modes_init = {}
+response_passenger_cars = session.post(url_passenger_cars, json=passenger_cars_query)
+response_heavy_cars = session.post(url_heavy_cars, json=heavy_cars_query)
 
-# loop through the response and save energy mode values to dictionary for passenger cars
-for i in response_json['data']:
+response_json_passenger_cars = json.loads(response_passenger_cars.content.decode('utf-8-sig'))
+response_json_heavy_cars = json.loads(response_heavy_cars.content.decode('utf-8-sig'))
+
+#print("PASSENGER CARS:")
+#print(response_json_passenger_cars)
+#print("HEAVY CARS:")
+#print(response_json_heavy_cars)
+
+# Create a initial dictionaries for parsing JSON responses
+passenger_cars_energy_modes = {}
+heavy_cars_energy_modes = {}
+
+# loop through responses and save energy mode values to dictionary
+# passenger cars
+for i in response_json_passenger_cars['data']:
     key = i["key"][3]
     value = i["values"][0]
     if value == "-":
-        energy_modes_init[key] = 0
+        passenger_cars_energy_modes[key] = 0
     else:
-        energy_modes_init[key] = value
+        passenger_cars_energy_modes[key] = value
+
+# heavy cars <-- here the fact that stats are by a quarter requires aggregation
+# This is still a direct copy from passenger cars and won't produce correct results
+# it won't give an error though, just wrong result
+for i in response_json_heavy_cars['data']:
+    key = i["key"][3]
+    value = i["values"][0]
+    if value == "-":
+        heavy_cars_energy_modes[key] = 0
+    else:
+        heavy_cars_energy_modes[key] = value
 
 # Check initial dictionary contents
-#print(energy_modes_init)
+#print(passenger_cars_energy_modes)
+#print(heavy_cars_energy_modes)
 
 # Create a refined dictionary with proper key names, energy mode combinations and proportional shares for passenger cars
-energy_modes_ref = {}
-energy_modes_ref["kvoima_bensiini"] = int(energy_modes_init["01"]) / int(energy_modes_init["YH"])
-energy_modes_ref["kvoima_diesel"] = int(energy_modes_init["02"]) / int(energy_modes_init["YH"])
-energy_modes_ref["kvoima_etanoli"] = int(energy_modes_init["37"]) / int(energy_modes_init["YH"])
-energy_modes_ref["kvoima_kaasu"] = (int(energy_modes_init["06"]) + int(energy_modes_init["11"]) + int(energy_modes_init["13"])) / int(energy_modes_init["YH"])
-energy_modes_ref["kvoima_phev_b"] = int(energy_modes_init["39"]) / int(energy_modes_init["YH"])
-energy_modes_ref["kvoima_phev_d"] = int(energy_modes_init["44"]) / int(energy_modes_init["YH"])
-energy_modes_ref["kvoima_ev"] = int(energy_modes_init["04"]) / int(energy_modes_init["YH"])
-energy_modes_ref["kvoima_vety"] = int(energy_modes_init["05"]) / int(energy_modes_init["YH"])
-energy_modes_ref["kvoima_muut"] = (int(energy_modes_init["33"]) + int(energy_modes_init["34"]) + int(energy_modes_init["38"]) + int(energy_modes_init["40"]) + int(energy_modes_init["41"]) + int(energy_modes_init["42"]) + int(energy_modes_init["43"]) + int(energy_modes_init["Y"])) / int(energy_modes_init["YH"])
+passenger_car_energy_modes_ref = {}
+passenger_car_energy_modes_ref["kvoima_bensiini"] = int(passenger_cars_energy_modes["01"]) / int(passenger_cars_energy_modes["YH"])
+passenger_car_energy_modes_ref["kvoima_diesel"] = int(passenger_cars_energy_modes["02"]) / int(passenger_cars_energy_modes["YH"])
+passenger_car_energy_modes_ref["kvoima_etanoli"] = int(passenger_cars_energy_modes["37"]) / int(passenger_cars_energy_modes["YH"])
+passenger_car_energy_modes_ref["kvoima_kaasu"] = (int(passenger_cars_energy_modes["06"]) + int(passenger_cars_energy_modes["11"]) + int(passenger_cars_energy_modes["13"])) / int(passenger_cars_energy_modes["YH"])
+passenger_car_energy_modes_ref["kvoima_phev_b"] = int(passenger_cars_energy_modes["39"]) / int(passenger_cars_energy_modes["YH"])
+passenger_car_energy_modes_ref["kvoima_phev_d"] = int(passenger_cars_energy_modes["44"]) / int(passenger_cars_energy_modes["YH"])
+passenger_car_energy_modes_ref["kvoima_ev"] = int(passenger_cars_energy_modes["04"]) / int(passenger_cars_energy_modes["YH"])
+passenger_car_energy_modes_ref["kvoima_vety"] = int(passenger_cars_energy_modes["05"]) / int(passenger_cars_energy_modes["YH"])
+passenger_car_energy_modes_ref["kvoima_muut"] = (int(passenger_cars_energy_modes["33"]) + int(passenger_cars_energy_modes["34"]) + int(passenger_cars_energy_modes["38"]) + int(passenger_cars_energy_modes["40"]) + int(passenger_cars_energy_modes["41"]) + int(passenger_cars_energy_modes["42"]) + int(passenger_cars_energy_modes["43"]) + int(passenger_cars_energy_modes["Y"])) / int(passenger_cars_energy_modes["YH"])
 
 # Check refined dictionary contents
-print(energy_modes_ref)
+#print(energy_modes_ref)
+
 
 # Add dictionary contents to database
-
 # Connect to database
 try:
     conn = psycopg2.connect(cfg.postgresql_string())
 except: 
     raise Exception("Couldn't connect to database")
-
 cursor = conn.cursor()
 
 # Create a new table
@@ -127,9 +125,9 @@ cursor.execute(create_table)
 insert_into = ("""INSERT INTO energy_modes(mun, scenario, year, kmuoto , kvoima_bensiini, kvoima_diesel, kvoima_etanoli, kvoima_kaasu, kvoima_phev_b, kvoima_phev_d, 
               kvoima_ev, kvoima_vety, kvoima_muut) 
               VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""")
-cursor.execute(insert_into, (mun_code, 'wem', 2021, 'hlauto', energy_modes_ref["kvoima_bensiini"], energy_modes_ref["kvoima_diesel"], energy_modes_ref["kvoima_etanoli"], 
-              energy_modes_ref["kvoima_kaasu"], energy_modes_ref["kvoima_phev_b"], energy_modes_ref["kvoima_phev_d"], energy_modes_ref["kvoima_ev"],
-              energy_modes_ref["kvoima_vety"], energy_modes_ref["kvoima_muut"]))
+cursor.execute(insert_into, (mun_code, 'wem', 2021, 'hlauto', passenger_car_energy_modes_ref["kvoima_bensiini"], passenger_car_energy_modes_ref["kvoima_diesel"], passenger_car_energy_modes_ref["kvoima_etanoli"], 
+              passenger_car_energy_modes_ref["kvoima_kaasu"], passenger_car_energy_modes_ref["kvoima_phev_b"], passenger_car_energy_modes_ref["kvoima_phev_d"], passenger_car_energy_modes_ref["kvoima_ev"],
+              passenger_car_energy_modes_ref["kvoima_vety"], passenger_car_energy_modes_ref["kvoima_muut"]))
 
 
 # Insert walking and biking
@@ -156,3 +154,8 @@ cursor.execute(insert_into, (mun_code, 'wem', 2021,'raide', 0, 0, 0, 0, 0, 0, 1,
 conn.commit()
 cursor.close()
 conn.close()
+
+# Tell user that no errors were encountered
+print()
+print("No errors, check Postgres localhost")
+print()
