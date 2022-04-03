3f9814b5ea7524fa7214cdab13cbacdebbc0691b
Ville Koivisto <ville.koivisto@ubigu.fi>
2022-04-03 17:37:38 +0300

    Update get_co2datafi.py
    - Get material resourceId, type, name and gwp values to a dictionary

--- data/get_co2datafi.py
+++ data/get_co2datafi.py
@@ -9,21 +9,24 @@ url = "https://co2data.fi/api/co2data_construction.json"
 session = requests.Session()
 response = session.post(url)
 
+# create output dictionary for gwp values (both materials and processes)
+gwp_values = {}
 
-# Make this into a loop to a dictionary
+# get JSON response from the SYKE service
 response_json = json.loads(response.content.decode('utf-8-sig'))
-print("Resource ID:")
-print(response_json["Resources"][0]["ResourceId"])
-print()
-print(response_json["Resources"][0]["Name"])
-print()
-print("Conservative GWP:")
-print(response_json["Resources"][0]["DataItems"]["DataValueItems"][0]["Value"])
-print()
-print("Typical GWP:")
-print(response_json["Resources"][0]["DataItems"]["DataValueItems"][1]["Value"])
 
-# parse the above dictionary as/if needed
 
+# loop through the response
+# for now we take only materials and exclude processes due to processes not having always two gwp values in 'DataValueItems'
+for i in range(len(response_json["Resources"])):
+    if response_json["Resources"][i]["ResourceType"] == "Material":
+        gwp_values[response_json["Resources"][i]["ResourceId"]] = []
+        gwp_values[response_json["Resources"][i]["ResourceId"]].append(response_json["Resources"][i]["Name"])
+        gwp_values[response_json["Resources"][i]["ResourceId"]].append(response_json["Resources"][i]["ResourceType"])
+        gwp_values[response_json["Resources"][i]["ResourceId"]].append(response_json["Resources"][i]["DataItems"]["DataValueItems"][0]["Value"])
+        gwp_values[response_json["Resources"][i]["ResourceId"]].append(response_json["Resources"][i]["DataItems"]["DataValueItems"][1]["Value"])
+
+#print(gwp_values)
 
 # push parsed contents to postgres. Try to use same database_handler.py
+# before doing this it would be better to create initial schema in pg data modeler, let's not create tables here unnecessary
