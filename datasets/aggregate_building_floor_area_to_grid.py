d53954d098c684ae7b8432e6c54b238d7e2cf3d0
koivusto <koivusto@gmail.com>
2022-07-30 23:05:02 +0300

    wip: buildings and materials co2 emission total
    - shortened building types in materials_per_building.json
    - shortened building types in building_type_classification.py
    - wip: aggregate_building_floor_area_to_grid

--- datasets/aggregate_building_floor_area_to_grid.py
+++ datasets/aggregate_building_floor_area_to_grid.py
@@ -1,7 +1,10 @@
 import geopandas as gpd
+from pandas import read_sql
+import json
 from sqlalchemy import create_engine
 from modules.config import Config
 from modules.building_type_classification import building_type_level1_1994
+import time
 #import matplotlib.pyplot as plt <-- uncomment if you want to visualize dataframe for testing
 
 '''
@@ -13,6 +16,9 @@ by grid cell, building type and fuel and construction year.
 The result is then pushed back to postgres (without geometry).
 '''
 
+# track time for completing the script
+start_time = time.time()
+
 # create config object for database
 cfg = Config()
 
@@ -23,20 +29,30 @@ engine = create_engine(cfg._db_connection_url("local_dev"))
 db_connection_url = cfg._db_connection_url("local_dev")
 con = create_engine(db_connection_url) 
 
-# get buildings from postgis
-sql_buildings = "SELECT * FROM data.rakennukset LIMIT 10"
-#WHERE id = 33000 OR ID = 33002 OR ID = 11328
+# get co2data from posgres (note: no geometry field)
+sql_co2data = "SELECT * FROM data.co2data_building_materials"
+df_co2data = read_sql(sql_co2data, con)
+
+# load material map per building type (json)
+with open('datasets/materials_per_building.json') as f:
+   data = json.load(f)
+
+# get buildings from postgres
+sql_buildings = "SELECT * FROM data.rakennukset"
 building_geom_col = 'geometry'
 df_buildings = gpd.read_postgis(sql_buildings, con, geom_col=building_geom_col)  
-#print(data_buildings.head())
 
 # transform building geometries to centroid
 df_buildings["geometry"] = df_buildings["geometry"].centroid
 
-# get grid from postgis
-sql_grid = "SELECT * FROM data.fi_grid_250m WHERE xyind = '3663756679375' OR xyind = '3733756685375' OR xyind = '3666256673875' OR xyind = '3763756680375' OR xyind = '3668756673375'"
+# get grid from postgres
+sql_grid = "SELECT * FROM data.fi_grid_250m"
 grid_geom_column = 'wkb_geometry'
 df_grid = gpd.read_postgis(sql_grid, con, geom_col=grid_geom_column)
+
+# limit grid to buildings extent using coordinate based index (reference: https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoDataFrame.cx.html)
+bbox = df_buildings.total_bounds
+df_grid = df_grid.cx[bbox[0]:bbox[2], bbox[1]:bbox[3]]
 
 # spatial join xyind from grid to building centroids
 df_buildings_with_xyind = df_buildings.sjoin(df_grid, how="left", predicate="within")
@@ -49,18 +65,49 @@ df_buildings_with_xyind[cfg.year_attribute()] = [((x//10)*10) for x in df_buildi
 df_buildings_with_xyind[cfg.building_code_attribute()] = [building_type_level1_1994(x) for x in df_buildings_with_xyind[cfg.building_code_attribute()]]
 
 # create a new dataframe in which floor area is grouped by grid cell, decade, building type and fuel type
-df_grid_with_agg_floor_area = df_buildings_with_xyind.groupby(['xyind',cfg.year_attribute(), cfg.building_code_attribute(), cfg.fuel_attribute()])[cfg.floor_area_attribute()].aggregate('sum').reset_index(name="floor_area_sum")
+df_bu_grouped = df_buildings_with_xyind.groupby(['xyind',cfg.year_attribute(), cfg.building_code_attribute(), cfg.fuel_attribute()])[cfg.floor_area_attribute()].aggregate('sum').reset_index(name="floor_area_sum")
 
 # rename columns
-df_grid_with_agg_floor_area = df_grid_with_agg_floor_area.rename(columns={cfg.year_attribute():"decade",cfg.building_code_attribute():"building_type",cfg.fuel_attribute():"fuel"})
+df_bu_grouped = df_bu_grouped.rename(columns={cfg.year_attribute():"decade",cfg.building_code_attribute():"building_type",cfg.fuel_attribute():"fuel"})
 
 # add 1 to index so that it won't start from 0
-df_grid_with_agg_floor_area.index += 1
+df_bu_grouped.index += 1
+
+# In some cases the original data might have had 0 values for floor area so we delete them here
+df_bu_grouped = df_bu_grouped.drop(df_bu_grouped[df_bu_grouped.floor_area_sum == 0].index)
+
+# function calculating total co2 emission for one square meter in specific building type
+def calc_material_co2(type:str):
+    co2_emission_m2 = 0.0
+    # loop through json
+    for key, value in data[type]["Materials_kg"].items():
+        gwp_typicalValue =0.0
+        # check if gwp_typical exists. Note that here is assumed that key is integer in dataframe and string in json file. 
+        if df_co2data.loc[(df_co2data['resourceid']==int(key)),'gwp_typical'].values.size > 0 :
+            gwp_typicalValue=df_co2data.loc[(df_co2data['resourceid']==int(key)),'gwp_typical'].values[0]
+        co2_emission_m2 += value * gwp_typicalValue
+    return co2_emission_m2 
+
+# get unique building types
+type_list = df_bu_grouped.building_type.unique()
+
+# loop through the type list and for all rows in each type calculate co2 emissions
+for i in type_list:
+    df_bu_grouped.loc[df_bu_grouped.building_type==i, 'co2_emission'] = calc_material_co2(i) * df_bu_grouped['floor_area_sum']
+
+# group by xyind
+
+# get geometry back on or go back and keep it along
 
 # push result back to postgis
 engine = create_engine(cfg._db_connection_url("local_dev"))
-df_grid_with_agg_floor_area.to_sql("grid_with_agg_floor_area",engine, schema="data",if_exists="replace", index_label="id")
+df_bu_grouped.to_sql("grid_with_agg_floor_area",engine, schema="data",if_exists="replace", index_label="id")
 
 # set id field as identity in postgres
 with engine.connect() as con_pk:
     con_pk.execute('ALTER TABLE data.grid_with_agg_floor_area ALTER "id" SET NOT NULL, ALTER "id" ADD GENERATED ALWAYS AS IDENTITY')
+
+
+# matplotlib script for checking the result?
+
+print(f"The script took {(time.time()-start_time)//60} minutes+ to run")
--- datasets/modules/building_type_classification.py
+++ datasets/modules/building_type_classification.py
@@ -1,19 +1,19 @@
 rakennusluokitus_1994 = {
-                        "A1: Erilliset pientalot":['011', '012','013'],
-                        "A2: Rivi- ja ketjutalot": ['021','022'],
-                        "A3: Asuinkerrostalot": ['032', '039'],
-                        "B: Vapaa-ajan asuinrakennukset":['041'],
-                        "C: Liikerakennukset":['111','112','119','121','123','124','129', '131','139','141'],
-                        "D: Toimistorakennukset":['151'],
-                        "E: Liikenteen rakennukset":['161','162','163','164','169'],
-                        "F: Hoitoalan rakennukset":['211','213','214','215','219','221','222','223','229','231','239','241'],
-                        "G: Kokoontumisrakennukset":['311','312', '322','323','324', '331', '341','342','349', '351','352','353','354','359', '369'],
-                        "H: Opetusrakennukset":['511', '521', '531','532', '541','549'],
-                        "J: Teollisuusrakennukset":['611','613','691','692','699'],
-                        "K: Varastorakennukset":['711','712','719'],
-                        "L: Palo- ja pelastustoimen rakennukset":['721','722','729'],
-                        "M: Maatalousrakennukset":['811','819','891','892','893','899'],
-                        "N: Muut rakennukset":['931','941','999']
+                        "A1":['011', '012','013'],
+                        "A2": ['021','022'],
+                        "A3": ['032', '039'],
+                        "B":['041'],
+                        "C":['111','112','119','121','123','124','129', '131','139','141'],
+                        "D":['151'],
+                        "E":['161','162','163','164','169'],
+                        "F":['211','213','214','215','219','221','222','223','229','231','239','241'],
+                        "G":['311','312', '322','323','324', '331', '341','342','349', '351','352','353','354','359', '369'],
+                        "H":['511', '521', '531','532', '541','549'],
+                        "J":['611','613','691','692','699'],
+                        "K":['711','712','719'],
+                        "L":['721','722','729'],
+                        "M":['811','819','891','892','893','899'],
+                        "N":['931','941','999']
                          }
                     
 def building_type_level1_1994(building:str):
@@ -32,7 +32,7 @@ def building_type_level1_1994(building:str):
             return key
     
     # if value was not found in the classification return "Other buildings" 
-    return "N: Muut rakennukset"
+    return "N"
 
 if __name__ == "__main__":
     print(building_type_level1_1994('699'))
