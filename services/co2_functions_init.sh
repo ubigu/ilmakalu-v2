327fb020ad7a6652aa80379014a3e0c80b117eec
Esa Östring <esa.ostring@ubigu.fi>
2022-09-21 13:54:38 +0300

    Insert database functions

---
+++ services/co2_functions_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# init CO2 -functions to local database
+# Note: ugly hack, since we would like to have this in docker-compose in a long run
+
+file=co2_functions.txt
+
+DBUSER=ilmakalu
+DBPASS=ilmakalu
+DBPORT=5435
+DB=ilmakalu
+
+while read -r line;
+do
+    echo "# Line is: $line"
+    echo "PGPASSWORD=${DBPASS} psql -U $DBUSER -h localhost -p $DBPORT $DB -f $line"
+done < $file
