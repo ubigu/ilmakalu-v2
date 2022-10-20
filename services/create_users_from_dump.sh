c91393c6753d6c80dcc91cadbcb570b50433106d
Esa Östring <esa.ostring@ubigu.fi>
2022-10-20 18:07:30 +0300

    Instructions to initialize database from a dump
    * cloud users not stripped
    * user has rights to create schema and table to schema

--- services/create_users_from_dump.sh
+++ services/create_users_from_dump.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-    echo "Give password for all users"
-    exit 1
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <password> <ilmakalu_user>"
+        exit 1
 fi
 infile="../datasets/dump_data/emissiontest_stripped.sql"
 cat $infile | egrep 'OWNER|GRANT' | sed -n 's/^.*TO \([a-z]*\);/\1/p'| sort | uniq |\
-    grep -v postgres | sed "s/^/CREATE USER /; s/$/ WITH PASSWORD '$1';/"
+    grep -v postgres |\
+    grep -v "cloudsqlsuperuser" |\
+    sed "s/^/CREATE USER /; s/$/ WITH PASSWORD '$1';/"
+
+echo "CREATE USER cloudsqladmin WITH PASSWORD '$1';"
+echo "CREATE USER cloudsqlsuperuser WITH SUPERUSER PASSWORD '$1';"
+echo "GRANT schemacreator TO $2;"
+# TODO: change organisation name default string
+echo "CREATE GROUP organisationname;"
+echo "GRANT organisationname TO $2;"
+echo "GRANT tablecreator TO $2;"
