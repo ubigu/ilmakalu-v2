c173e72ff38a85b4930c5745c6d385fb36bce576
Esa Östring <esa.ostring@ubigu.fi>
2022-10-25 07:57:57 +0300

    Grid table with municipality
    * modified to reflect original grid

--- datasets/sql/municipality_grid.sql
+++ datasets/sql/municipality_grid.sql
@@ -2,25 +2,21 @@ DROP TABLE IF EXISTS data.fi_grid_municipalities;
 
 CREATE TABLE IF NOT EXISTS data.fi_grid_municipalities (
     fid SERIAL PRIMARY KEY,
-    xyind CHAR(14),
-    natcode CHAR(3),
-    namefin VARCHAR(60),
-    nameswe VARCHAR(60),
+    xyind CHAR(13),
+    mun CHAR(3),
+    zone BIGINT,
+    centdist FLOAT,
     geom Geometry(Polygon, 3067)
 );
 
 INSERT INTO data.fi_grid_municipalities(
     xyind,
-    natcode,
-    namefin,
-    nameswe,
+    mun,
     geom
 )
 SELECT
     g.xyind,
     m.natcode,
-    m.namefin,
-    m.nameswe,
     g.wkb_geometry
 FROM
     data.fi_grid_250m AS g,
