c3516f6099cdcf0c6b017e399da7c10c0ca09383
Ville Koivisto <ville.koivisto@ubigu.fi>
2023-03-16 10:57:04 +0200

    Update create_end_users.sh
    Change connection string so that the database is correct

--- azure/create_end_users.sh
+++ azure/create_end_users.sh
@@ -6,19 +6,19 @@ set -e
 . ./azure_variables.sh "$1"
 
 # create end user role and make admin role a member of it
-psql "$conn_string" <<-EOSQL
+psql "$conn_string_adm_ilmakalu_data" <<-EOSQL
     CREATE GROUP "end_users" WITH NOLOGIN;
     GRANT "end_users" TO ${DATA_USER};
     GRANT "end_users" TO ${ADMIN_USER};
 EOSQL
 
-# create (or replace) function and event trigger for ownership transfer
-psql "$conn_string" -f ./sql_files/table_owner_transfer_trigger.sql
+# create event trigger for ownership transfer
+psql "$conn_string_adm_ilmakalu_data" -f ./sql_files/table_owner_transfer_trigger.sql
 
 # Give the group required privileges in compute schemas
 for schema in aluejaot built delineations energy functions grid_globals traffic
 do
-    psql "$conn_string" <<-EOSQL
+    psql "$conn_string_adm_ilmakalu_data" <<-EOSQL
     GRANT USAGE ON SCHEMA $schema TO "end_users";
     GRANT SELECT ON ALL TABLES IN SCHEMA $schema TO "end_users";
     ALTER DEFAULT PRIVILEGES IN SCHEMA $schema GRANT SELECT ON TABLES TO "end_users";
@@ -28,7 +28,7 @@ done
 # Give the group required privileges in user schemas
 for schema in user_input user_output
 do
-    psql "$conn_string" <<-EOSQL
+    psql "$conn_string_adm_ilmakalu_data" <<-EOSQL
     GRANT ALL ON SCHEMA $schema TO "end_users";
     GRANT SELECT ON ALL TABLES IN SCHEMA $schema TO "end_users";
     ALTER DEFAULT PRIVILEGES IN SCHEMA $schema GRANT SELECT ON TABLES TO "end_users";
@@ -39,7 +39,7 @@ done
 for end_user in $END_USERS
 do
     end_user_pw=$(openssl rand -base64 12)
-    psql "$conn_string" <<-EOSQL
+    psql "$conn_string_adm_ilmakalu_data" <<-EOSQL
     CREATE USER ${end_user} WITH PASSWORD '${end_user_pw}';
     GRANT "end_users" TO ${end_user};
 EOSQL
