d4fc9bf8c232657bd5502201dec751f168cf04c4
Ville Koivisto <ville.koivisto@ubigu.fi>
2023-03-19 16:24:53 +0200

    Update create_end_users.sh
    Separate user group role and trigger creation to a another file

--- azure/create_end_users.sh
+++ azure/create_end_users.sh
@@ -5,37 +5,7 @@ set -e
 # obtain variables
 . ./azure_variables.sh "$1"
 
-# create end user role and make admin role a member of it
-psql "$conn_string_adm_ilmakalu_data" <<-EOSQL
-    CREATE GROUP "end_users" WITH NOLOGIN;
-    GRANT "end_users" TO ${DATA_USER};
-    GRANT "end_users" TO ${ADMIN_USER};
-EOSQL
-
-# create event trigger for ownership transfer
-psql "$conn_string_adm_ilmakalu_data" -f ./sql_files/table_owner_transfer_trigger.sql
-
-# Give the group required privileges in compute schemas
-for schema in aluejaot built delineations energy functions grid_globals traffic
-do
-    psql "$conn_string_adm_ilmakalu_data" <<-EOSQL
-    GRANT USAGE ON SCHEMA $schema TO "end_users";
-    GRANT SELECT ON ALL TABLES IN SCHEMA $schema TO "end_users";
-    ALTER DEFAULT PRIVILEGES IN SCHEMA $schema GRANT SELECT ON TABLES TO "end_users";
-EOSQL
-done
-
-# Give the group required privileges in user schemas
-for schema in user_input user_output
-do
-    psql "$conn_string_adm_ilmakalu_data" <<-EOSQL
-    GRANT ALL ON SCHEMA $schema TO "end_users";
-    GRANT SELECT ON ALL TABLES IN SCHEMA $schema TO "end_users";
-    ALTER DEFAULT PRIVILEGES IN SCHEMA $schema GRANT SELECT ON TABLES TO "end_users";
-EOSQL
-done
-
-# create users one by one and assign them to end user group
+# create users one by one, assign them to end user group and alter their search path
 for end_user in $END_USERS
 do
     end_user_pw=$(openssl rand -base64 12)
