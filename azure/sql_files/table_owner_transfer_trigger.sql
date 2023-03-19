9e5ebbf105a23c23d7291c2ac139e9b8c874f47c
Ville Koivisto <ville.koivisto@ubigu.fi>
2023-03-19 16:28:31 +0200

    Update table_owner_transfer_trigger.sql
    Run event trigger contents as invoker in oppose to definer since other way around causes errors in ownership.
    
    Remove if statements since end users are not privileged to create objects in other schemas anyway.

--- azure/sql_files/table_owner_transfer_trigger.sql
+++ azure/sql_files/table_owner_transfer_trigger.sql
@@ -3,26 +3,20 @@ BEGIN;
 CREATE OR REPLACE FUNCTION trigger_create_set_table_owner()
  RETURNS event_trigger
  LANGUAGE plpgsql
- SECURITY DEFINER -- run as function owner which is superuser
- SET search_path = admin, pg_temp -- make sure that pg_temp is not utilized
+ --SECURITY DEFINER -- run contents with function owner privileges
+ --SET search_path = admin, pg_temp -- make sure that pg_temp is not utilized
 AS $$
 DECLARE
   obj record;
 BEGIN
   FOR obj IN SELECT * FROM pg_event_trigger_ddl_commands() WHERE command_tag='CREATE TABLE' LOOP
-    IF obj.schema_name='user_input' OR obj.schema_name='user_output'
-    THEN EXECUTE format('ALTER TABLE %s OWNER TO end_users', obj.object_identity);
-    END IF;
+    EXECUTE format('ALTER TABLE %s OWNER TO end_users', obj.object_identity);
   END LOOP;
   FOR obj IN SELECT * FROM pg_event_trigger_ddl_commands() WHERE command_tag='CREATE SEQUENCE' LOOP
-    IF obj.schema_name='user_input' OR obj.schema_name='user_output'
-    THEN EXECUTE format('ALTER SEQUENCE %s OWNER TO end_users', obj.object_identity);
-    END IF;
+    EXECUTE format('ALTER SEQUENCE %s OWNER TO end_users', obj.object_identity);
   END LOOP;
   FOR obj IN SELECT * FROM pg_event_trigger_ddl_commands() WHERE command_tag='CREATE FUNCTION' LOOP
-    IF obj.schema_name='user_input' OR obj.schema_name='user_output'
-    THEN EXECUTE format('ALTER FUNCTION %s OWNER TO end_users', obj.object_identity);
-    END IF;
+    EXECUTE format('ALTER FUNCTION %s OWNER TO end_users', obj.object_identity);
   END LOOP;
 END;
 $$;
@@ -35,4 +29,5 @@ CREATE EVENT TRIGGER trigger_create_set_table_owner
 
 REVOKE ALL ON FUNCTION trigger_create_set_table_owner() FROM PUBLIC;
 GRANT EXECUTE ON FUNCTION trigger_create_set_table_owner() TO "end_users";
+
 COMMIT;
