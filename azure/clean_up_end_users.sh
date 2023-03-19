99fbc77e41046868f07929195b042d1a654d38e5
Ville Koivisto <ville.koivisto@ubigu.fi>
2023-03-19 16:24:09 +0200

    Update clean_up_end_users.sh
    Clean up only end users, leave group role and trigger to be

--- azure/clean_up_end_users.sh
+++ azure/clean_up_end_users.sh
@@ -5,22 +5,10 @@ set -e
 # obtain variables
 . ./azure_variables.sh "$1"
 
-# remove user group
-psql "$conn_string" <<-EOSQL
-    DROP OWNED BY "end_users";
-    DROP USER "end_users";
-EOSQL
-
-# clean up end users and resources they own
+# clean up end users
 for end_user in $END_USERS
 do
-    psql "$conn_string" <<-EOSQL
+    psql "$conn_string_adm_ilmakalu_data" <<-EOSQL
     DROP USER $end_user;
 EOSQL
 done
-
-# remove event trigger
-psql "$conn_string" <<-EOSQL
-    DROP FUNCTION IF EXISTS trigger_create_set_table_owner CASCADE; 
-    DROP EVENT TRIGGER IF EXISTS trigger_create_set_table_owner CASCADE;
-EOSQL
