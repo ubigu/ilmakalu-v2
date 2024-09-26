79200ecfb3b0df83fab29084d6820e6bc30f3ac1
Toni <toni.kuikka@ubigu.fi>
2024-09-26 11:04:25 +0300

    Enable XML and GeoJSON output formats

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,20 +1,39 @@
 import os
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Response
 from sqlmodel import SQLModel, Session, create_engine, text
 from typing import Annotated
 from datetime import datetime
+import pandas as pd
+import geopandas as gp
 
 app = FastAPI()
 DATABASE_URL = os.environ.get('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 
 from models import built, delineations, energy, grid_globals, traffic
-
 SQLModel.metadata.create_all(engine)
 
-def execute(stmt):
+def execute(stmt, outputformat):
+    if type(outputformat) == str:
+        outputformat = outputformat.lower()
+
     with Session(engine) as session:
-        return session.exec(stmt).mappings().all()
+        all =  session.exec(stmt).mappings().all()
+        data = pd.DataFrame.from_records(all)
+        match outputformat:
+            case 'xml':
+                return Response(
+                    content=data.to_xml(index=False),
+                    media_type="application/xml"
+                )
+            case 'geojson':
+                data['geom'] = gp.GeoSeries.from_wkt(data['geom'])
+                return Response(
+                    content=gp.GeoDataFrame(data, geometry='geom', crs="EPSG:3067").to_json(drop_id=True),
+                    media_type="application/geo+json"
+                )
+            case _:
+                return all
     
 @app.get("/co2-calculate-emissions/")
 def CO2_CalculateEmissions(
@@ -30,15 +49,19 @@ def CO2_CalculateEmissions(
     targetYear: int | None = None,
     plan_areas: str | None = None,
     plan_centers: str | None = None,
-    plan_transit: str | None = None 
+    plan_transit: str | None = None,
+    outputformat: str | None = None
 ):
     stmt = text(
         """SELECT
-            ST_AsText(geom), xyind, mun, zone, year, floorspace,
-            pop, employ, tilat_vesi_tco2, tilat_lammitys_tco2, tilat_jaahdytys_tco2,
-            sahko_kiinteistot_tco2, sahko_kotitaloudet_tco2, sahko_palv_tco2,
-            sahko_tv_tco2, liikenne_as_tco2, liikenne_tp_tco2, liikenne_tv_tco2,
-            liikenne_palv_tco2, rak_korjaussaneeraus_tco2, rak_purku_tco2,
+            ST_AsText(geom) as geom, xyind, mun, zone,
+            date_part('year', year) as year, floorspace, pop,
+            employ, tilat_vesi_tco2, tilat_lammitys_tco2,
+            tilat_jaahdytys_tco2, sahko_kiinteistot_tco2,
+            sahko_kotitaloudet_tco2, sahko_palv_tco2,
+            sahko_tv_tco2, liikenne_as_tco2, liikenne_tp_tco2,
+            liikenne_tv_tco2, liikenne_palv_tco2,
+            rak_korjaussaneeraus_tco2, rak_purku_tco2,
             rak_uudis_tco2, sum_yhteensa_tco2, sum_lammonsaato_tco2,
             sum_liikenne_tco2, sum_sahko_tco2, sum_rakentaminen_tco2
         FROM functions.CO2_CalculateEmissions(
@@ -72,7 +95,7 @@ def CO2_CalculateEmissions(
         plan_centers=plan_centers,
         plan_transit=plan_transit
     )
-    return execute(stmt)
+    return execute(stmt, outputformat)
     
 @app.get("/co2-calculate-emissions-loop/")
 def CO2_CalculateEmissionsLoop(
@@ -87,15 +110,19 @@ def CO2_CalculateEmissionsLoop(
     targetYear: int | None = None,
     plan_areas: str | None = None,
     plan_centers: str | None = None,
-    plan_transit: str | None = None 
+    plan_transit: str | None = None,
+    outputformat: str | None = None
 ):
     stmt = text(
         """SELECT
-            ST_AsText(geom), xyind, mun, zone, year, floorspace,
-            pop, employ, tilat_vesi_tco2, tilat_lammitys_tco2, tilat_jaahdytys_tco2,
-            sahko_kiinteistot_tco2, sahko_kotitaloudet_tco2, sahko_palv_tco2,
-            sahko_tv_tco2, liikenne_as_tco2, liikenne_tp_tco2, liikenne_tv_tco2,
-            liikenne_palv_tco2, rak_korjaussaneeraus_tco2, rak_purku_tco2,
+            ST_AsText(geom) as geom, xyind, mun, zone,
+            date_part('year', year) as year, floorspace, pop,
+            employ, tilat_vesi_tco2, tilat_lammitys_tco2,
+            tilat_jaahdytys_tco2, sahko_kiinteistot_tco2,
+            sahko_kotitaloudet_tco2, sahko_palv_tco2,
+            sahko_tv_tco2, liikenne_as_tco2, liikenne_tp_tco2,
+            liikenne_tv_tco2, liikenne_palv_tco2,
+            rak_korjaussaneeraus_tco2, rak_purku_tco2,
             rak_uudis_tco2, sum_yhteensa_tco2, sum_lammonsaato_tco2,
             sum_liikenne_tco2, sum_sahko_tco2, sum_rakentaminen_tco2
         FROM functions.CO2_CalculateEmissionsLoop(
@@ -126,28 +153,5 @@ def CO2_CalculateEmissionsLoop(
         plan_centers=plan_centers,
         plan_transit=plan_transit
     )
-    return execute(stmt)
-    
-@app.get("/co2-build_construct/")
-def CO2_BuildConstruct(
-    floorSpace: float | None = None,
-    year: Annotated[list[int], Query()] | None = None,
-    buildingType: str | None = None,
-    calculationScenario: str | None = None
-):
-    stmt = text(
-        """SELECT * FROM functions.CO2_BuildConstruct(
-            floorSpace => :floorSpace,
-            calculationYears => :calculationYears,
-            buildingType => :buildingType,
-            calculationScenario => :calculationScenario
-        );"""
-    ).bindparams(
-        floorSpace=floorSpace,
-        calculationYears=year,
-        buildingType=buildingType,
-        calculationScenario=calculationScenario
-    )
-    return execute(stmt)
-    
+    return execute(stmt, outputformat)
     
