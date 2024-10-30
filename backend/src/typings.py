5385d7e804f7e4a2ab7c65132996bd44fdcaf23c
Toni <toni.kuikka@ubigu.fi>
2024-10-30 16:13:25 +0200

    Export the GeoJSON types

--- backend/src/typings.py
+++ backend/src/typings.py
@@ -1,49 +1,13 @@
-from typing import Literal, NotRequired, TypedDict
+from typing import TypedDict
+
+from pygeojson import Feature, FeatureCollection
 
 
-class Point(TypedDict):
-    type: Literal["Point"]
-    coordinates: list[float]
-
-
-class LineString(TypedDict):
-    type: Literal["LineString"]
-    coordinates: list[list[float]]
-
-
-class Polygon(TypedDict):
-    type: Literal["Polygon"]
-    coordinates: list[list[list[float]]]
-
-
-class MultiPoint(TypedDict):
-    type: Literal["MultiPoint"]
-    coordinates: list[list[float]]
-
-
-class MultiLineString(TypedDict):
-    type: Literal["MultiLineString"]
-    coordinates: list[list[list[float]]]
-
-
-class MultiPolygon(TypedDict):
-    type: Literal["MultiPolygon"]
-    coordinates: list[list[list[list[float]]]]
-
-
-class Feature(TypedDict):
-    geometry: Point | LineString | Polygon | MultiPoint | MultiLineString | MultiPolygon
-    id: NotRequired[str | int]
-    properties: dict
-
-
-class FeatureCollection(TypedDict):
-    type: Literal["FeatureCollection"]
-    features: list[Feature]
+class InputLayer(TypedDict):
+    name: str
+    base: str
+    features: FeatureCollection | list[Feature]
 
 
 class UserInput(TypedDict):
-    aoi: NotRequired[list[Feature] | FeatureCollection]
-    plan_areas: NotRequired[list[Feature] | FeatureCollection]
-    plan_transit: NotRequired[list[Feature] | FeatureCollection]
-    plan_centers: NotRequired[list[Feature] | FeatureCollection]
+    layers: list[InputLayer]
