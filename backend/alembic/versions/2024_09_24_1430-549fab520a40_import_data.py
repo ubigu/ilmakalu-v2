3842bfee6647131d34f726cebe1ed3383bdb3d84
Toni <toni.kuikka@ubigu.fi>
2024-12-16 16:15:54 +0200

    Import centroids from API

--- backend/alembic/versions/2024_09_24_1430-549fab520a40_import_data.py
+++ backend/alembic/versions/2024_09_24_1430-549fab520a40_import_data.py
@@ -6,10 +6,14 @@ Create Date: 2024-09-24 14:30:03.831130
 
 """
 
+import io
 import os
 from csv import DictReader
 from typing import Sequence, Union
 
+import fiona
+import geopandas as gpd
+import requests
 import sqlalchemy as sa
 import sqlmodel
 
@@ -24,27 +28,28 @@ depends_on: Union[str, Sequence[str], None] = None
 tables = sqlmodel.SQLModel.metadata.tables
 delimiter = ";"
 encoding = "utf-8-sig"
+geom_col = "geom"
 
 
-def __import_from_sql(file, connection):
+def __import_from_sql(conn, file):
     try:
         with open(file) as f:
             stmt = sa.sql.text(f.read())
-            with connection.begin_nested():
-                connection.execute(stmt)
+            with conn.begin_nested():
+                conn.execute(stmt)
     except Exception:
         return
 
 
-def __import_from_csv(file, name, schema, connection):
+def __import_from_csv(conn, file, name, schema):
     """It is assumed that the file path follows
     the pattern '{root_dir}/{schema}/{table_name}.csv'"""
     table_name = f"{schema}.{name}"
     try:
         with open(file, encoding=encoding) as f:
-            with connection.begin_nested():
+            with conn.begin_nested():
                 stmt = f"COPY {table_name} FROM STDIN DELIMITER '{delimiter}' CSV HEADER;"
-                connection.connection.cursor().copy_expert(stmt, f)
+                conn.connection.cursor().copy_expert(stmt, f)
     except Exception:
         # If COPY fails, try to insert rows one by one (this is slow)
         if table_name not in tables:
@@ -53,30 +58,49 @@ def __import_from_csv(file, name, schema, connection):
         with open(file, encoding=encoding) as f:
             for row in DictReader(f, skipinitialspace=True, delimiter=delimiter):
                 try:
-                    with connection.begin_nested():
-                        connection.execute(table.insert().values({k: v for k, v in row.items()}))
+                    with conn.begin_nested():
+                        conn.execute(table.insert().values({k: v for k, v in row.items()}))
                 except Exception:
                     continue
 
 
+def __import_centroids(conn):
+    __update_delineations_srid("centroids")
+
+    r = requests.get("https://wwwd3.ymparisto.fi/d3/gis_data/spesific/KeskustaAlueet2021.zip")
+    if not r.ok:
+        raise OSError("Failed to fetch the centroid data from the API")
+    with fiona.BytesCollection(io.BytesIO(r.content).read()) as src:
+        gdf = gpd.GeoDataFrame.from_features(src, crs=src.crs).rename_geometry(geom_col)
+        gdf[geom_col] = gdf[geom_col].centroid
+        gdf["id"] = gdf.index + 1
+        gdf = gdf[[geom_col, "id", "Keskustyyp", "KeskusNimi"]]
+        gdf.columns = [c.lower() for c in gdf.columns]
+        gdf.to_postgis("centroids", con=conn, if_exists="append", schema="delineations")
+
+
+def __update_delineations_srid(tbl):
+    op.execute(f"SELECT UpdateGeometrySRID('delineations','{tbl}','{geom_col}',3067)")
+
+
 def upgrade() -> None:
-    connection = op.get_bind()
-    root_dir = "database"
-    for subdir, _, files in os.walk(root_dir):
+    conn = op.get_bind()
+    __import_centroids(conn)
+
+    for subdir, _, files in os.walk("database"):
         for file_name in files:
             file = os.path.join(subdir, file_name)
             print(f"Importing from file {file}...")
             name, ext = os.path.splitext(os.fsdecode(file_name))
             match ext:
                 case ".sql":
-                    __import_from_sql(file, connection)
+                    __import_from_sql(conn, file)
                 case ".csv":
                     dir = os.path.basename(os.path.normpath(subdir))
-                    __import_from_csv(file, name, dir, connection)
+                    __import_from_csv(conn, file, name, dir)
                 case _:
                     continue
-    op.execute("SELECT UpdateGeometrySRID('delineations','centroids','geom',3067)")
-    op.execute("SELECT UpdateGeometrySRID('delineations','grid','geom',3067)")
+    __update_delineations_srid("grid")
 
 
 def downgrade() -> None:
