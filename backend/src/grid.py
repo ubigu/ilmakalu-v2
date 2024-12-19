bc2f9df0cda931390c89a5ed272985c619bc3d20
Toni <toni.kuikka@ubigu.fi>
2024-12-19 09:32:17 +0200

    Generate grid squares in a function

--- backend/src/grid.py
+++ backend/src/grid.py
@@ -5,6 +5,7 @@ import geopandas as gpd
 import pandas as pd
 import requests
 from shapely import distance
+from shapely.geometry import MultiPolygon
 from shapely.ops import nearest_points
 
 zone_mappings = {
@@ -41,23 +42,27 @@ def __import_ykr_zones(df, filename, target_col):
     return df
 
 
+def __point_to_multipolygon(x, y):
+    """Convert the bottom-left coordinate to a 250x250-square"""
+    return MultiPolygon([(((x, y), (x, y + 250), (x + 250, y + 250), (x + 250, y)), [])])
+
+
 def generate_grid(mun, centroids):
     mun_col = "kunnro2024"
-    hila250m_link = pd.read_csv(
+    geom_col = "geom"
+
+    grid = pd.read_csv(
         f"https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:hila250m_linkki&outputFormat=csv&cql_filter={mun_col}={mun}"
     ).drop_duplicates(subset=["euref_x", "euref_y"])
-    hila250m = gpd.read_file("data/hila250m_kaikki.gpkg")
 
-    grid = pd.merge(hila250m_link, hila250m, how="left", on=["euref_x", "euref_y"])[["geometry", mun_col]].rename(
-        columns={"geometry": "geom", mun_col: "mun"}
-    )
-    grid = gpd.GeoDataFrame(grid, geometry="geom", crs="epsg:3067")
+    grid[geom_col] = grid.apply(lambda g: __point_to_multipolygon(g["euref_x"], g["euref_y"]), axis=1)
+    grid = gpd.GeoDataFrame(grid, geometry=geom_col, crs="epsg:3067").rename(columns={mun_col: "mun"})
 
     # Generate xyind by concatenating the x and y coordinates of the square's centroid (EUREF-FIN EPSG:3067)
-    grid["xyind"] = grid["geom"].centroid.apply(lambda c: str(round(c.x)) + str(round(c.y)))
+    grid["xyind"] = grid[geom_col].centroid.apply(lambda c: str(round(c.x)) + str(round(c.y)))
 
     # Calculate the squares' distances to the nearest centroids in the centroids table in kilometers
-    grid["centdist"] = grid["geom"].centroid.apply(
+    grid["centdist"] = grid[geom_col].centroid.apply(
         lambda c: round(distance(c, nearest_points(c, centroids.geom.union_all())[1]) * 0.001)
     )
 
@@ -68,4 +73,4 @@ def generate_grid(mun, centroids):
         lambda g: zone_mappings[g["vyohselite"] if isinstance(g["vyohselite"], str) else g["Nimi"]], axis=1
     )
 
-    return grid[["geom", "xyind", "mun", "zone", "centdist"]]
+    return grid[[geom_col, "xyind", "mun", "zone", "centdist"]]
