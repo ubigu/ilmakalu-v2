19a197f8d50b00e30a7fa7d4ff4b44ca3d66a58c
Ilpo Tammi <33652320+iltapommi@users.noreply.github.com>
2025-03-27 20:57:44 +0200

    Remove obsolete comments

--- backend/database/functions/co2_buildconstruct.sql
+++ backend/database/functions/co2_buildconstruct.sql
@@ -1,15 +1,8 @@
 /* Uudisrakentaminen (energia) | Construction of new buildings (energy)
-
 YKR-ruudun laskentavuoden uudisrakentamisen energian kasvihuonekaasupäästöt [CO2-ekv/a] lasketaan seuraavasti:
-
     rak_uusi_energia_co2 = rakennus_ala * rak_energia_gco2m2
-
 YKR-ruudun laskentavuoden uudisrakentamisen rakennustuotteiden kasvihuonekaasupäästöt rak_uusi_materia_co2 [CO2-ekv/a] lasketaan seuraavasti:
- 
-    rak_uusi_materia_co2 = rakennus_ala * rak_materia_gco2m2
-
-
-*/
+    rak_uusi_materia_co2 = rakennus_ala * rak_materia_gco2m2 */
 CREATE SCHEMA IF NOT EXISTS functions;
 DROP FUNCTION IF EXISTS functions.CO2_BuildConstruct;
 CREATE OR REPLACE FUNCTION
@@ -29,39 +22,27 @@ BEGIN
 /* Returning zero, if grid cell has 0, -1 or NULL built floor area */
 IF floorSpace <= 0 OR floorSpace IS NULL THEN
     RETURN 0;
-/* In other cases continue with the calculation */
 ELSE
 
-    /* If the year of construction is not the current year of calculation, return 0 */
---      IF rakennusvuosi != year THEN
---        RETURN 0;
-        /* Muussa tapauksessa jatka laskentaan */
-        /* In other cases, continue */
---    ELSE
-
-        /* Haetaan laskentavuoden ja kehitysskenaarion perusteella rakennustyyppikohtaiset uudisrakentamisen energiankulutuksen kasvihuonekaasupäästöt */
-        /* Get the unit emissions for energy consumption of construction by year of building, scenario and building type */
-        EXECUTE 'SELECT ' || buildingType || ' FROM built.build_new_construction_energy_gco2m2
+    /* Haetaan laskentavuoden ja kehitysskenaarion perusteella rakennustyyppikohtaiset uudisrakentamisen energiankulutuksen kasvihuonekaasupäästöt */
+    EXECUTE 'SELECT ' || buildingType || ' FROM built.build_new_construction_energy_gco2m2
+        WHERE year = $1 AND scenario = $2 LIMIT 1'
+        INTO construction_energy_gco2m2 USING calculationYear, calculationScenario;
+    
+    /* Haetaan laskentavuoden ja kehitysskenaarion perusteella rakennustyyppikohtaiset uudisrakentamisen materiaalien valmistuksen kasvihuonekaasupäästöt */
+    EXECUTE 'SELECT ' || CASE WHEN buildingType IN ('erpien', 'rivita', 'askert')
+        THEN buildingType
+            ELSE 'muut'
+        END || ' 
+            FROM built.build_materia_gco2m2
             WHERE year = $1 AND scenario = $2 LIMIT 1'
-            INTO construction_energy_gco2m2 USING calculationYear, calculationScenario;
-        
-        /* Haetaan laskentavuoden ja kehitysskenaarion perusteella rakennustyyppikohtaiset uudisrakentamisen materiaalien valmistuksen kasvihuonekaasupäästöt */
-        /* Get the unit emissions for production of materials for construction by year of building, scenario and building type */
-        EXECUTE 'SELECT ' || CASE WHEN buildingType IN ('erpien', 'rivita', 'askert')
-            THEN buildingType
-                ELSE 'muut'
-            END || ' 
-                FROM built.build_materia_gco2m2
-                WHERE year = $1 AND scenario = $2 LIMIT 1'
-            INTO construction_materials_gco2m2
-                USING calculationYear, calculationScenario;
+        INTO construction_materials_gco2m2
+            USING calculationYear, calculationScenario;
 
-        /* Lasketaan ja palautetaan päästöt CO2-ekvivalenttia [gCO2-ekv/v] */
-        /* Calculate and return emissions as CO2-equivalents [gCO2-ekv/a] */
-        RETURN floorSpace * 
-            (COALESCE(construction_energy_gco2m2,0) + COALESCE(construction_materials_gco2m2, 0));
+    /* Lasketaan ja palautetaan päästöt CO2-ekvivalenttia [gCO2-ekv/v] */
+    RETURN floorSpace * 
+        (COALESCE(construction_energy_gco2m2,0) + COALESCE(construction_materials_gco2m2, 0));
 
---    END IF;
 END IF;
 
 END;
